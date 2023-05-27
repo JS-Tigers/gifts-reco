2d95aa2856470cae15b1ef5fd8e1781ac7a71dac
shobhit-bhatt-14 <shobhit.bhatt.14@gmail.com>
2023-05-27 16:33:16 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 // react
 import { useState } from "react";
 // mui
-import { Box, FormControl, FormControlLabel, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, Typography } from "@mui/material";
+import { Autocomplete, Box, Button, Card, Chip, FormControl, FormControlLabel, FormLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, TextField, useMediaQuery } from "@mui/material";
 
 export default function Home() {
   const [gender, setGender] = useState(null);
@@ -15,32 +15,55 @@ export default function Home() {
     setAge(event.target.value);
   };
 
+  const breakpoint = useMediaQuery("max-width: 720px");
+
   return (
     <Box
       sx={{
         height: "100%",
         width: "100%",
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "var(--dark)",
       }}
     >
-      <FormControl fullWidth>
-        <FormLabel sx={{ fontWeight: "bold" }}>Gender</FormLabel>
-        <RadioGroup row value={gender} onChange={genderChangeHandler}>
-          <FormControlLabel value="boy" control={<Radio />} label="Boy" />
-          <FormControlLabel value="girl" control={<Radio />} label="Girl" />
-          <FormControlLabel value="both" control={<Radio />} label="Both" />
-        </RadioGroup>
-      </FormControl>
-      <FormControl fullWidth>
-        <InputLabel sx={{ fontWeight: "bold" }}>Age</InputLabel>
-        <Select value={age} label="Age" onChange={ageChangeHandler}>
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
-        </Select>
-      </FormControl>
+      <Card variant="outlined" raised sx={{ width: 500, padding: 3 }}>
+        <FormControl>
+          <FormLabel sx={{ fontWeight: "bold", textAlign: "center" }}>Gender</FormLabel>
+          <RadioGroup row value={gender} onChange={genderChangeHandler}>
+            <FormControlLabel value="boy" control={<Radio />} label="Boy" />
+            <FormControlLabel value="girl" control={<Radio />} label="Girl" />
+            <FormControlLabel value="both" control={<Radio />} label="Both" />
+          </RadioGroup>
+        </FormControl>
+        <FormControl fullWidth>
+          <InputLabel sx={{ fontWeight: "bold" }}>Age</InputLabel>
+          <Select value={age} label="Age" onChange={ageChangeHandler}>
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+        <Autocomplete
+          freeSolo
+          multiple
+          options={[]}
+          defaultValue={[]}
+          renderTags={(value, getTagProps) => value.map((option, index) => <Chip variant="outlined" label={option} {...getTagProps({ index })} />)}
+          renderInput={(params) => <TextField {...params} label="Likes" placeholder="(optional)" />}
+        />
+        <Autocomplete
+          freeSolo
+          multiple
+          options={[]}
+          defaultValue={[]}
+          renderTags={(value, getTagProps) => value.map((option, index) => <Chip variant="outlined" label={option} {...getTagProps({ index })} />)}
+          renderInput={(params) => <TextField {...params} label="Dislikes" placeholder="(optional)" />}
+        />
+        <Button variant="contained">Submit</Button>
+      </Card>
     </Box>
   );
 }
